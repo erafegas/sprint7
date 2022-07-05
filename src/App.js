6c1f6cb16ed81ab0b9db4cddd3c0201a740738a6
Eduard <erafegas@gmail.com>
2022-07-05 17:57:16 +0200

    Exercici 2

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
 import './app.css';
-import { useState } from "react";
+import Panel from "./components/Panel";
 
 
   function App() {
      
-    const [total, setTotal] = useState(0);
-    
-    
-    const handleChange = (event) => {
-
-
-      let isCheck = event.target.checked; // true false
-      let nameCheckbox = event.target.name; // seo ads web
-
-      const preus = { web: 500, seo: 300, ads: 200};
-      let count = preus[nameCheckbox]
-
-      if (isCheck) {
-        setTotal(total + count);
-      }
-      else {
-        setTotal(total - count);
-      }
-
-    }
-   
 
   return (
     <div className="App"> 
-      <p> Que quieres hacer? </p>
-        <ul>
-          <li className='llista'> 
-            <input type='checkbox' className='llista-cb' name='web' onChange={handleChange}/> 
-            <label htmlFor ="web">  Una pàgina web (500€)  </label>
-          </li>
-          <li className='llista'> 
-            <input type='checkbox' className='llista-cb' name='seo' onChange={handleChange}/>  
-            <label htmlFor ="seo">  Una consultoria SEO (300€)  </label>
-          </li>
-          <li className='llista'> 
-            <input type='checkbox' className='llista-cb' name='ads' onChange={handleChange}/>  
-            <label htmlFor ="ads">  Una campanya de Google Ads (200€)  </label>
-          </li>
-        </ul>
-        <div>
-          <ul className="total"> <li> Total: {total} €</li></ul>
-        </div>
+      <Panel/>
     </div>
   );
 }
---
+++ src/components/Panel.js
@@ -0,0 +1,103 @@
+import { Fragment, useState}  from "react";
+import styled from "styled-components";
+
+
+const InputsPanel = styled.div `
+width:350px;
+height: 100px;
+border: 1px solid black;  
+border-radius: 20px;
+display: flex;
+flex-wrap: wrap;
+align-items: center;
+justify-content: center;
+`;    
+
+
+const  Panel =  () => {
+
+  const preus = { web: 500, seo: 300, ads: 200};
+
+  const [total, setTotal] = useState(0);
+  //inputs show
+  const [inputs, setInputs] = useState('none');
+  //inputs calc
+  const [paginas, setPaginas] = useState(0);
+  const [idiomas, setIdiomas] = useState(0);
+
+  const handleChangeInputs = (valor , nom) => {
+
+      if ( nom === "pags") {
+        setPaginas(valor)
+        setTotal(preus.web + (valor * idiomas * 30));
+      }
+      else if ( nom === "idi") {
+        setIdiomas(valor)
+        setTotal(preus.web + (valor * paginas * 30));
+      }
+  }
+
+  const handleChange = (event) => {
+
+  let isCheck = event.target.checked; // true false
+  let nameCheckbox = event.target.name; // seo ads web
+  let preuServei = preus[nameCheckbox];
+
+  if (isCheck) {
+    setTotal(total + preuServei);
+    if (nameCheckbox === 'web') {
+      setInputs('block');
+      setTotal(total + preuServei);
+    }
+  }
+  else {
+      if (nameCheckbox === 'web') {
+        setInputs('none');
+        setTotal(total - preuServei);
+      }
+      else {
+              setTotal(total - preuServei);
+      }
+    }
+  } 
+  return (
+    <Fragment> 
+      <p> Que quieres hacer? </p>
+      <ul>
+        <li className='llista'> 
+          <input type='checkbox' className='llista-cb' name='web' onChange={handleChange}/> 
+          <label htmlFor ="web"> Una pàgina web (500€) </label>
+        </li>
+        <div style={{ display: inputs}}>
+          <InputsPanel >
+            <label htmlFor="pags"> Número de páginas </label> 
+              <input type='text' name="pags" onChange={(event) => {
+                const valor = event.target.value
+                const nom = event.target.name
+                handleChangeInputs(valor, nom)
+              }}/> 
+            <label htmlFor="lng"> Número de idiomas  </label> 
+              <input type='text' name="idi" onChange={(event) => {
+                const valor = event.target.value
+                const nom = event.target.name
+                handleChangeInputs(valor, nom)
+              }}/> 
+          </InputsPanel> 
+        </div>
+        <li className='llista'> 
+          <input type='checkbox' className='llista-cb' name='seo' onChange={handleChange}/>  
+          <label htmlFor ="seo">  Una consultoria SEO (300€)  </label>
+        </li>
+        <li className='llista'> 
+          <input type='checkbox' className='llista-cb' name='ads' onChange={handleChange}/>  
+          <label htmlFor ="ads">  Una campanya de Google Ads (200€)  </label>
+        </li>
+      </ul>
+      <div>
+        <ul className="total"> <li> Total: {total} €</li></ul>
+      </div>
+    </Fragment>
+  )
+}
+
+export default Panel;
